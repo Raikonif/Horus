fa7c35a9781b9490d6b58a92a3daea7263215900
Raikonif <raikonif@gmail.com>
2021-10-03 20:46:23 -0400

    database postgresql added

--- horusapp/settings/local.py
+++ horusapp/settings/local.py
@@ -8,8 +8,12 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR.child('db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'horusdb',
+        'USER': 'developer',
+        'PASSWORD': 'developer',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
