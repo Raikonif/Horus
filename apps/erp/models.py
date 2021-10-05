8b29ea04da7d2d6708a78f6d9f2e9b0d5a4db657
Raikonif <raikonif@gmail.com>
2021-10-05 19:53:22 -0400

    models classes created

--- apps/erp/models.py
+++ apps/erp/models.py
@@ -2,14 +2,39 @@ from django.db import models
 from datetime import datetime
 
 # Create your models here.
+class Type(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Tipo')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Tipo'
+        verbose_name_plural = 'Tipos'
+        ordering = ['id']    
+
+class Category(models.Model):
+    name = models.CharField(max_length=150,  verbose_name='Nombre')
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+        ordering = ['id']
+    
+
 class Employee(models.Model):
+    categ = models.ManyToManyField(Category)
+    type = models.ForeignKey(Type, on_delete=models.CASCADE)
     names = models.CharField(max_length=150, verbose_name='Nombres')
     dni = models.CharField(max_length=50, verbose_name='C.I.')
     date_joined = models.DateField(default=datetime.now, verbose_name='Fecha de registro')
     date_creation = models.DateTimeField(auto_now=True)
     date_updated = models.DateTimeField(auto_now_add=True)
     age = models.PositiveIntegerField(default=0)
-    gender = models.CharField(max_length=50)
+    # gender = models.CharField(max_length=50)
     salary = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
     state = models.BooleanField(default=True)
     avatar = models.ImageField(upload_to='avatar/%Y/%m/%d', null=True, blank=True)
--- apps/erp/tests.py
+++ apps/erp/tests.py
@@ -1,3 +1,3 @@
 from django.test import TestCase
-
+from apps.erp import Type
 # Create your tests here.
